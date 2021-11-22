ca8c2c2158b5ce432b1f46b593552de5b3e3193a
Thomas J Varghese <tvarghese@medicompinc.com>
2021-11-22 11:59:43 +0530

    Ignore testinf.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
+import pymongo
 import numpy as np
 import pickle
+import ssl
 
 model = pickle.load(open("models/cardio.pkl", "rb"))
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/mydb"
+client = pymongo.MongoClient("mongodb+srv://thomas123:5799@cardiodb.sz4px.mongodb.net/CardioDB?retryWrites=true&w=majority", ssl=True,
+ssl_cert_reqs=ssl.CERT_NONE)
+db = client.get_database("Card")
 mongo = PyMongo(app)
 user_collection = mongo.db.users
 
@@ -16,7 +20,7 @@ def new():
     return render_template("ui.html")
 
 
-'''
+"""
 @app.route("/data", methods=['GET'])
 def show_data():
     if request.method == 'GET':
@@ -38,13 +42,13 @@ def show_data():
     else:
         return ("Kindly fill the form")
 
-'''
+"""
 
 
 @app.route("/read")
 def read_data():
-    users = (user_collection.find())
-    return render_template('index.html', users=users)
+    users = user_collection.find()
+    return render_template("index.html", users=users)
 
 
 @app.route("/predict", methods=["POST", "GET"])
@@ -76,24 +80,40 @@ def predict():
                 datax
             )
 
-    if datax != "" and data1 != "" and data2 != "" and data3!="" and data4!="" and data5!="" and data6!="" and data7!=""and data8!=""and data9!=""and data10!=""and data11!="" :
-        users = user_collection.insert_one({"name": datax, "age":data1, "gender": data2, "height": data3, "weight": data4, "syst_pressure":data5, "diast_pressure":data6, "cholestrol":data7, "glucose":data8, "smoker":data9, "alcoholic": data10, "active":data11})
-        #return ("data added to the database")
+    if (
+        datax != ""
+        and data1 != ""
+        and data2 != ""
+        and data3 != ""
+        and data4 != ""
+        and data5 != ""
+        and data6 != ""
+        and data7 != ""
+        and data8 != ""
+        and data9 != ""
+        and data10 != ""
+        and data11 != ""
+    ):
+        users = user_collection.insert_one(
+            {
+                "name": datax,
+                "age": data1,
+                "gender": data2,
+                "height": data3,
+                "weight": data4,
+                "syst_pressure": data5,
+                "diast_pressure": data6,
+                "cholestrol": data7,
+                "glucose": data8,
+                "smoker": data9,
+                "alcoholic": data10,
+                "active": data11,
+            }
+        )
+        # return ("data added to the database")
 
     return render_template("ui.html", statement=statement())
 
 
 if __name__ == "__main__":
     app.run()
-
-
-
-
-
-
-
-
-
-
-
-
